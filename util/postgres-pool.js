07a726df9e90e8f93bad0b6f1a28959aefdc756a
jacknolan <921Jack@gmail.com>
2021-11-30 22:08:37 -0500

    Add SSL (hopefully)

--- util/postgres-pool.js
+++ util/postgres-pool.js
@@ -4,7 +4,10 @@ const { Pool } = require('pg')
 let pool;
 if (process.env.DATABASE_URL) {
     pool = new Pool({
-        connectionString: process.env.DATABASE_URL
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
 } else {
     pool = new Pool({
